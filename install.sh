4c5fe4d49ec4dc4994787ed18982d08fc2a665a1
Dexy <helpdextron@gmail.com>
2023-06-22 22:58:21 +0200

    small updates

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
-pacman -S --noconfirm archlinux-keyring 
 pacman -S - < pkglist.txt
-
+pacman -S --noconfirm archlinux-keyring 
 # sets dhcp and enables it:
 pacman -S --noconfirm --needed networkmanager dhclient
 systemctl enable --now NetworkManager
 # set keyboard layout:
 loadkeys no-latin1
-
 # sets ntp:
 timedatectl set-ntp true
 # Sets timezone
 timedatectl --no-ask-password set-timezone Europe/Oslo
 
-# Enables Login display manager (SDDM)
+# Enables Login display manager
 sudo systemctl enable --now sddm.service
 
 mkdir -p ~/Pictures/Screenshots
 cp .config $home/
 
 Echo "Config copied and services enabled"
+
+Hyprland
