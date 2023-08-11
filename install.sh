37a5a096312d4d6c7e787d7af92daddb86757238
Dexy <helpdextron@gmail.com>
2023-08-11 19:39:28 +0200

    updated files

--- install.sh
+++ install.sh
@@ -8,13 +8,21 @@ loadkeys no-latin1
 # sets ntp:
 timedatectl set-ntp true
 # Sets timezone
+sleep 5
+echo "Timezone Set to Europe/Oslo"
 timedatectl --no-ask-password set-timezone Europe/Oslo
 
 # Enables Login display manager
 sudo systemctl enable --now sddm.service
-echo export $(run-parts /usr/lib/systemd/user-environment-generators | sed '/:$/d; /^$/d' | xargs) > ~/.bash.profile
+echo export $(run-parts /usr/lib/systemd/user-environment-generators | sed '/:$/d; /^$/d' | xargs) > ~/.bash_profile
 mkdir -p ~/Pictures/Screenshots
 mv .config ../
+echo "Configs Moved"
 
-su dexy
-Hyprland
+mv hypr.desktop /usr/share/wayland-sessions/hypr.desktop
+echo ".desktop file moved"
+
+mv Hypr.sh > /usr/local/bin/
+echo "Hpyr config moved"
+
+sudo -u dexy -H sh -c "Hyprland"
