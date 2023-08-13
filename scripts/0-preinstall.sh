af2cfaf5680f206043ed7e330fe820f542a5ba61
Dexy <helpdextron@gmail.com>
2023-08-13 02:10:13 +0200

    updated

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source ./settings
 timedatectl set-ntp true
 sed -i 's/^#Para/Para/' /etc/pacman.conf
-pacman -S --noconfirm curl rsync grub unzip
+pacman -S --noconfirm curl rsync grub unzip parted
 
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 echo -e "-------------------------------------------------------------------------"
@@ -28,7 +28,6 @@ else
 fi
 cd /
 
-mkdir /mnt
 mkdir /mnt/root
 mkdir /mnt/root/dexyarch
 
@@ -39,36 +38,39 @@ echo "--------------------------------------"
 echo -e "\nFormatting disk...\n$HR"
 echo "--------------------------------------"
 #read -p "continue?" con
+umount -A --recursive /mnt # make sure everything is unmounted before we start
 # disk prep
 sgdisk -Z ${DISK} # zap all on disk
 sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
 
 # create partitions
 sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
-sgdisk -n 2::+500M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
-sgdisk -n 3::+5G --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, minus 10G for swap
-if [[ ! -d "/sys/firmware/efi" ]]; then
+sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
+sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
+if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
     sgdisk -A 1:set:2 ${DISK}
 fi
+partprobe ${DISK} # reread partition table to ensure it is correct
+
 
 #read -p "continue?" con
 
 # make filesystems
 echo -e "\nCreating Filesystems...\n$HR"
-if [[ ${DISK} =~ "nvme" ]]; then
-mkfs.vfat -F32 -n "EFIBOOT" "${DISK}p2"
-mkfs.btrfs -L "ROOT" "/dev/sda3" -F
-mount "/dev/sda3" /mnt
-
-else
-mkfs.vfat -F32 -n "EFIBOOT" "${DISK}2"
-mkfs.btrfs -L "ROOT" "/dev/sda3" -F
-mount "/dev/sda3" /mnt
+if [[ "${FS}" == "btrfs" ]]; then
+    mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
+    mkfs.btrfs -L ROOT ${partition3} -f
+    mount -t btrfs ${partition3} /mnt
+elif [[ "${FS}" == "ext4" ]]; then
+    mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
+    mkfs.ext4 -L ROOT ${partition3}
+    mount -t ext4 ${partition3} /mnt
+mkfs.btrfs -L ROOT ${partition3}
 fi
 
 mkdir /mnt/boot
+mkdir -p /mnt/boot/efi
 mount -t vfat -L EFIBOOT /mnt/boot/
-mkdir /mnt/boot/efi
 
 if ! grep -qs '/mnt' /proc/mounts; then
     echo "Drive is not mounted can not continue"
@@ -83,8 +85,12 @@ read -p "ready to install. continue?" con
 echo "--------------------------------------"
 echo "-- Arch Install on Main Drive       --"
 echo "--------------------------------------"
-pacstrap /mnt base base-devel linux linux-firmware linux-headers git libnewt unzip --noconfirm --needed
-genfstab -U /mnt >> /mnt/etc/fstab
+pacstrap /mnt base base-devel linux linux-firmware linux-headers sudo git libnewt unzip --noconfirm --needed
+genfstab -L /mnt >> /mnt/etc/fstab
+echo " 
+  Generated /etc/fstab:
+"
+cat /mnt/etc/fstab
 cp -R /scripts/ /mnt/root/dexyarch
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 
