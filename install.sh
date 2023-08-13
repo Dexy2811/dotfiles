728eca06a7dc3f5ce6546386382e858a8186a656
Dexy <helpdextron@gmail.com>
2023-08-13 13:17:16 +0200

    updated disk formatting

--- install.sh
+++ install.sh
@@ -96,37 +96,79 @@ pacman -S --noconfirm gptfdisk
 echo "--------------------------------------"
 echo -e "\nFormatting disk...\n$HR"
 echo "--------------------------------------"
-#read -p "continue?" con
-# disk prep
-sgdisk -Z ${DISK} # zap all on disk
-sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+lsblk
+echo "Enter the drive to install arch linux on it. (/dev/...)"
+echo "Enter Drive (eg. /dev/sda or /dev/vda or /dev/nvme0n1 or something similar)"
+read drive
+sleep 2s
+clear
 
-# create partitions
-sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
-sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
-sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
-if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
-    sgdisk -A 1:set:2 ${DISK}
-fi
 
-#read -p "continue?" con
+lsblk
+echo "Choose a familier disk utility tool to partition your drive!"
+echo " 1. fdisk"
+echo " 2. cfdisk"
+echo " 3. gdisk"
+echo " 4. parted"
+read partitionutility
 
-# make filesystems
-echo -e "\nCreating Filesystems...\n$HR"
-if [[ ${DISK} =~ "nvme" ]]; then
-mkfs.vfat -F32 -n "EFIBOOT" "${DISK}p2"
-mkfs.btrfs -L "ROOT" "/dev/sda3" -F
-mount "/dev/sda3" /mnt
+case "$partitionutility" in
+  1 | fdisk | Fdisk | FDISK)
+  partitionutility="fdisk"
+  ;;
+  2 | cfdisk | Cfdisk | CFDISK)
+  partitionutility="cfdisk"
+  ;;
+  3 | gdisk | Gdisk | GDISK)
+  partitionutility="gdisk"
+  ;;
+  4 | parted | Parted | PARTED)
+  partitionutility="parted"
+  ;;
+  *)
+  echo "Unknown or unsupported disk utility! Default = cfdisk."
+  partitionutility="cfdisk"
+  ;;
+esac
+echo ""$partitionutility" is the selected disk utility tool for partition."
+sleep 3s
+clear
+echo "Getting ready for creating partitions!"
+echo "root and boot partitions are mandatory."
+echo "home and swap partitions are optional but recommended!"
+echo "Also, you can create a separate partition for timeshift backup (optional)!"
+echo "Getting ready in 9 seconds"
+sleep 9s
+"$partitionutility" "$drive"
+clear
+lsblk
+echo "choose your linux file system type for formatting drives"
+echo " 1. ext4"
+echo " 2. xfs"
+echo " 3. btrfs"
+echo " 4. f2fs"
+echo " Boot partition will be formatted in fat32 file system type."
+read filesystemtype
 
-else
-mkfs.vfat -F32 -n "EFIBOOT" "${DISK}2"
-mkfs.btrfs -L "ROOT" "/dev/sda3" -F
-mount "/dev/sda3" /mnt
-fi
-
-mkdir /mnt/boot
-mount -t vfat -L EFIBOOT /mnt/boot/
-mkdir /mnt/boot/efi
+case "$filesystemtype" in
+  1 | ext4 | Ext4 | EXT4)
+  filesystemtype="ext4"
+  ;;
+  2 | xfs | Xfs | XFS)
+  filesystemtype="xfs"
+  ;;
+  3 | btrfs | Btrfs | BTRFS)
+  filesystemtype="btrfs"
+  ;;
+  4 | f2fs | F2fs | F2FS)
+  filesystemtype="f2fs"
+  ;;
+  *)
+  echo "Unknown or unsupported Filesystem. Default = ext4."
+  filesystemtype="ext4"
+  ;;
+esac
+echo ""$filesystemtype" is the selected file system type."
 
 if ! grep -qs '/mnt' /proc/mounts; then
     echo "Drive is not mounted can not continue"
